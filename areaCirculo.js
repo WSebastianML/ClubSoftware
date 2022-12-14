409aeb06515274834806a507325b4910f859b20b
Sebastian <wsebastianmoyano@gmail.com>
2022-12-14 17:38:18 -0500

    cambiar tipo string a int

--- areaCirculo.js
+++ areaCirculo.js
@@ -1,2 +1,9 @@
-let r = process.argv [2] //toma el valor en la posicion 2 en el terminal
-console.log(Math.PI * (r**2))
+let r = parseInt(process.argv [2]) //toma el valor en la posicion 2 en el terminal
+function areaCirculo(r){
+    return Math.PI * (r ** 2)
+}
+let result = areaCirculo(3)
+console.log(typeof(r))
+console.log(typeof(process.argv))
+console.log(r + 4)
+console.log(areaCirculo(32))
