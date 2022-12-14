6d3f914ed19a556ac3976c4320beaa377c968fbd
Sebastian <wsebastianmoyano@gmail.com>
2022-12-14 17:50:14 -0500

    manejo de arreglos

---
+++ test.js
@@ -0,0 +1,8 @@
+let arr = [43, 23, 46, 10]
+console.log(typeof(arr))
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+console.log(arr.join(" "))
+console.log(arr.sort())
+console.log(arr.reverse())
