d78051366898f9cb91b028046f7572018674bc2b
Sebastian <wsebastianmoyano@gmail.com>
2023-01-04 17:25:33 -0500

    mayor entre 3 numeros

---
+++ max3.js
@@ -0,0 +1,11 @@
+function maximo(num1, num2, num3){
+    if((num1 > num2) && (num1 > num3)){
+        return num1
+    }else if(num2 > num3){
+        return num2
+    }else{
+        return num3
+    }
+}
+
+console.log(maximo(105, 75, 13))
